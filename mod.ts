e7ce355f76faf8b493da2b211802d1bbcdd05d72
Michael Ekstrand <md@ekstrandom.net>
2024-08-16 20:23:45 -0400

    drop @module doc from mod.ts

--- mod.ts
+++ mod.ts
@@ -1,8 +1,3 @@
-/**
- * Main entry point for the locking classes.
- *
- * @module
- */
 export { Mutex } from "./mutex.ts";
 export { Notify } from "./notify.ts";
 export { LockHandle, Semaphore } from "./semaphore.ts";
